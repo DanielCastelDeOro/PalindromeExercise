640b9351ea64733a8ed78d83c77e7b6ee477acff
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-08-07 13:15:34 -0600

    final commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+   public class WordSmith
+    {
+        public bool isAPalidrome(string word)
+        {
+            var rev = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                rev += word[i];
+            }
+            if (rev == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+            
+           // return word.EndsWith(word); word.StartsWith(word);
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("wow", true)]
+        [InlineData("racecar", true)]
+        [InlineData("people", false)]
+        public void PalidromeTest(string word, bool expected)
         {
-
-        }
+            var test = new WordSmith();
+            var actual = test.isAPalidrome(word);
+            Assert.Equal(expected, actual);
+        } 
     }
 }
